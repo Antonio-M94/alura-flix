669f48fe957bef2a05e5b04a63285e5773875a0b
Antonio-M94 <imondragonespinal@gmail.com>
2024-06-22 23:29:52 -0600

    header terminado

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import styles from './header.module.css';
+import logoAlura from '../../assets/logo-alura.png';
+
+const Header = () => {
+  return (
+    <header className={styles.container}>
+      <img src={logoAlura} alt="Logo Alura" />
+      <nav className={styles.navContainer}>
+        <button className={styles.home}>home</button>
+        <button className={styles.nuevoVideo}>nuevo video</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
